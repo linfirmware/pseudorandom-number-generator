912605aaefbf3d43a7ed4310c871fb122738745a
Wesley Carlsen <wcarlsen@linfirmware.com>
2016-02-03 20:08:09 -0500

    Refactor numbers tab

--- pseudorandom-number-generator/www/js/controllers.js
+++ pseudorandom-number-generator/www/js/controllers.js
@@ -1,17 +1,17 @@
 angular.module('starter.controllers', [])
 
-.controller('NumberGeneratorCtrl', function($scope, Chats) {
+.controller('NumberGeneratorCtrl', function($scope, GeneratedNumbers) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
   // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
+  $scope.$on('$ionicView.enter', function(e) {
+    $scope.generatedNumbers = GeneratedNumbers.all();
+  });
+
+  $scope.remove = function(numberSequence) {
+    GeneratedNumbers.remove(numberSequence);
   };
 })
 
